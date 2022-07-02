90a827a1b818fba0107424cc961ac08adfbc85d5
Eric Liu <ericliu4943@gmail.com>
2022-07-02 16:48:05 -0700

    Add react modal

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
 import "./styles/tailwind.css";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form, Field } from "formik";
+import { useState, useCallback } from "react";
+import Modal from "react-modal";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = useCallback(() => {
+    setIsOpen(true);
+  }, [setIsOpen]);
+
+  const closeModal = useCallback(() => {
+    setIsOpen(false);
+  }, [setIsOpen]);
+
+  console.log(isOpen);
   return (
     <div>
-      <h1>Validate it!</h1>
-      <Formik
-        initialValues={{ summary: "" }}
-        onSubmit={(values, { setSubmitting }) => {
-          console.log("submitting");
-          console.log({ values });
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form>
-            <Field type="text" name="summary" />
-            <button type="submit" disable={isSubmitting}>
-              Submit
-            </button>
-          </Form>
-        )}
-      </Formik>
-      <button onClick={() => console.log("hello")}>Accomplishment</button>
+      <Modal isOpen={isOpen}>
+        <h1>Validate it!</h1>
+        <Formik
+          initialValues={{ summary: "" }}
+          onSubmit={(values, { setSubmitting }) => {
+            closeModal();
+            console.log("submitting");
+            console.log({ values });
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <Field type="text" name="summary" />
+              <button type="submit" disabled={isSubmitting}>
+                Submit
+              </button>
+            </Form>
+          )}
+        </Formik>
+      </Modal>
+      <button onClick={openModal}>Accomplishment</button>
     </div>
   );
 }
